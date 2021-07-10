9a4c5c659a1198c210ecea32962892cb9398d39b
SimonaPentangelo <s.pentangelo4@studenti.unisa.it>
2021-07-10 21:16:12 +0200

    two sensors

--- src/garagehandler.js
+++ src/garagehandler.js
@@ -1,8 +1,10 @@
 const amqp = require("amqplib");
 require("dotenv").config({ path: ".env" });
 var nodemailer = require('nodemailer');
+var safe = -1;
 
 connectAndWait();
+connectAndWait_Two();
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -33,7 +35,9 @@ function responseGenerator(val) {
   var garage = parseInt(val.slice(0, 1));
   var someone = parseInt(val.toString().slice(1, 2));
   
-  if(garage == 1) {
+  if(someone == 2 && garage == 0) {
+    return "The garage is open 🔓, I'm not able to know if there's someone in my path, I can't close it. ⚠️";
+  } else if(garage == 1) {
     return "The garage is closed 🔒, I'll open it for you. 🔓";
   } else if(garage == 0 && someone == 1) {
     return "The garage is open 🔓, I can't close it, there's someone in my path... 🧍‍♂️";
@@ -42,50 +46,19 @@ function responseGenerator(val) {
   }
 }
 
-function connectAndWait() {
-  amqp
-    .connect(`amqp://guest:guest@${process.env.MY_IP}:5672`)
-    .then(function (conn) {
-      return conn.createChannel().then(function (ch) {
-        var ok = ch.assertQueue("iot/garagedoor", { durable: false });
-
-        ok = ok.then(function (_qok) {
-          return ch.consume(
-            "iot/garagedoor",
-            function (msg) {
-              console.log(msg.content.toString());
-              result = "*** This is an auto-generated message ***\n\n" +
-                        "Hi! I'm the garage handler!⚙️\n"; 
-              var val = connectAndWait_Two();
-              var string = msg.content.toString + val;
-              result += responseGenerator(string);
-              console.log(result);
-              sendMsg(result);
-            },
-            { noAck: true }
-          );
-        });
-
-        return ok.then(function (_consumeOk) {
-          console.log("*** First Sensor: ON ***");
-        });
-      });
-    })
-    .catch(console.warn);
-}
-
 function connectAndWait_Two() {
   amqp
     .connect(`amqp://guest:guest@${process.env.MY_IP}:5672`)
     .then(function (conn) {
       return conn.createChannel().then(function (ch) {
-        var ok = ch.assertQueue("iot/safesensor", { durable: false });
+        var ok = ch.assertQueue("iot/safety", { durable: false });
 
         ok = ok.then(function (_qok) {
           return ch.consume(
-            "iot/safesensor",
+            "iot/safety",
             function (msg) {
-              return msg.content.toString();
+              console.log("Second sensor result: " + msg.content.toString());
+              safe = msg.content.toString();
             },
             { noAck: true }
           );
@@ -93,6 +66,41 @@ function connectAndWait_Two() {
 
         return ok.then(function (_consumeOk) {
           console.log("*** Second Sensor: ON ***");
+        });
+      });
+    })
+    .catch(console.warn);
+}
+
+function connectAndWait() {
+  amqp
+    .connect(`amqp://guest:guest@${process.env.MY_IP}:5672`)
+    .then(function (conn) {
+      return conn.createChannel().then(function (ch) {
+        var ok = ch.assertQueue("iot/garagedoor", { durable: false });
+        ok = ok.then(function (_qok) {
+          return ch.consume(
+            "iot/garagedoor",
+            function (msg) {
+              console.log("First sensor result: " + msg.content.toString());
+              result = "*** This is an auto-generated message ***\n\n" +
+                        "Hi! I'm the garage handler!⚙️\n"; 
+              console.log("Valore safety: " + safe);
+              if(safe == -1) {
+                safe = 2
+              }
+              var s = msg.content.toString().concat(safe.toString());
+              result += responseGenerator(s);
+              console.log(result);
+              sendMsg(result);
+              safe = -1;
+            },
+            { noAck: true }
+          );
+        });
+
+        return ok.then(function (_consumeOk) {
+          console.log("*** First Sensor: ON ***");
         });
       });
     })
